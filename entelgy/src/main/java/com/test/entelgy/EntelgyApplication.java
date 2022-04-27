cff29bb3369ba679c540558312a36c1e9ea6b82a
berriose13 <52588808+berriose13@users.noreply.github.com>
2022-04-26 21:47:35 -0500

    Add files via upload

---
+++ entelgy/src/main/java/com/test/entelgy/EntelgyApplication.java
@@ -0,0 +1,13 @@
+package com.test.entelgy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntelgyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntelgyApplication.class, args);
+	}
+
+}
